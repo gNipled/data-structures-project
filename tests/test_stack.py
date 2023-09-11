1c9cfacd7c719bdfa4c13296d265225d635fc538
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-09-12 00:10:18 +0900

    wrote test for method push of Stack class coverage of stack.py is 92%

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+from src.stack import Node
+
+stack = Stack()
+
+
+class TestStack(unittest.TestCase):
+
+    def test_Stack_Push(self):
+        stack.push('data1')
+        stack.push('data2')
+        self.assertEquals(stack.top.data, 'data2')
+        self.assertEquals(stack.top.next_node.data, 'data1')
+        with self.assertRaises(AttributeError):
+            stack.top.next_node.next_node.data
