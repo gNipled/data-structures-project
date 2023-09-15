b63bad654c26d3139ae1b41f67674ce2e0cf4520
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-09-15 15:22:24 +0900

    Added test for new method pop. Coverage of stack.py is 100%

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,6 +4,7 @@ from src.stack import Stack
 from src.stack import Node
 
 stack = Stack()
+stack_1 = Stack()
 
 
 class TestStack(unittest.TestCase):
@@ -15,3 +16,12 @@ class TestStack(unittest.TestCase):
         self.assertEquals(stack.top.next_node.data, 'data1')
         with self.assertRaises(AttributeError):
             stack.top.next_node.next_node.data
+
+    def test_Stack_Pop(self):
+        stack_1.push('data1')
+        self.assertEquals(stack_1.pop(), 'data1')
+        self.assertIsNone(stack_1.top)
+        stack_1.push('data1')
+        stack_1.push('data2')
+        self.assertEquals(stack_1.pop(), 'data2')
+        self.assertEquals(stack_1.top.data, 'data1')
